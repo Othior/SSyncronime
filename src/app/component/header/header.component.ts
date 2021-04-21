340c0edea1c3d06356a9ee06f48a8b14c03a3797
Othior <splingaire.marvyn@gmail.com>
2021-04-21 16:39:27 +0200

    refactoring code header

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -17,8 +17,34 @@ export class HeaderComponent implements OnInit {
     this.isClicked ? check.add("activeNav") : check.remove("activeNav");
     console.log("check",check);
   }
+  list_link: Array<Link> = [
+    {
+      title:"Accueil",
+      link:"/accueil"
+    },
+    {
+      title:"Hypnose",
+      link:"/hypnose"
+    },
+    {
+      title:"Ateliers",
+      link:"/ateliers"
+    },
+    {
+      title:"Contact",
+      link:"/contact"
+    },
+    {
+      title:"Tarifs",
+      link:"/tarifs"
+    },
+  ]
 
   ngOnInit(): void {
   }
 
+}
+export interface Link{
+  title:string,
+  link:string
 }
