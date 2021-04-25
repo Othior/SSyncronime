f26e56436e4fec91982fdc8b5d5c44b006bd3d43
Othior <splingaire.marvyn@gmail.com>
2021-04-25 13:30:17 +0200

    correction text/ change content l'hypnose

--- src/app/component/differente-hypnose/differente-hypnose.component.ts
+++ src/app/component/differente-hypnose/differente-hypnose.component.ts
@@ -18,7 +18,7 @@ export class DifferenteHypnoseComponent implements OnInit {
       image:'../../../assets/hypnose1.jpg',
       name:'image hypnose perte de poids',
       title:"HYPNOSE TERAPEUTIQUE",
-      content:"L’hypnose thérapeutique considère le corps et l'esprit comme un tout où la mobilité corporelle va de pair avec le vécu émotionnel et la pensée.L\’hypnose thérapeutique incite la personne à lâcher prise par rapport aux blocages que son mental a installés, ce qui facilite la déprogrammation des anciens schémas de pensées négatives et la reconnexion à ses ressources profondes et sa sagesse intérieure.Les informations d’événements difficiles peuvent aussi être stockées dans la mémoire tissulaire. Grâce à l état de transe hypnotique et le contact thérapeutique du corps en tension, le thérapeute peut ainsi réveiller ces souvenirs et faciliter la communication entre l\'esprit, l\'émotion et le corps de la personne.Celle-ci entre alors en contact avec son « savoir intérieur »."
+      content:"L’hypnose thérapeutique considère le corps et l'esprit comme un tout où la mobilité corporelle va de pair avec le vécu émotionnel et la pensée. L\’hypnose thérapeutique incite la personne à lâcher prise par rapport aux blocages que son mental a installés, ce qui facilite la déprogrammation des anciens schémas de pensées négatives et la reconnexion à ses ressources profondes et sa sagesse intérieure. Les informations d’événements difficiles peuvent aussi être stockées dans la mémoire tissulaire. Grâce à l état de transe hypnotique et le contact thérapeutique du corps en tension, le thérapeute peut ainsi réveiller ces souvenirs et faciliter la communication entre l\'esprit, l\'émotion et le corps de la personne. Celle-ci entre alors en contact avec son « savoir intérieur »."
     },
     {
       image:'../../../assets/thinking-5968108_1920.jpg',
@@ -30,7 +30,7 @@ export class DifferenteHypnoseComponent implements OnInit {
       image:'https://cdn.pixabay.com/photo/2013/01/20/20/20/hypnosis-75615_960_720.jpg',
       name:'auto hypnose',
       title:'HYPNOSE SOMATO-MOTIONELLE',
-      content:'C’est une hypnose conversationnelle où le patient n’est pas endormi mais bien dans un état modifié de conscience dans lequel il est capable de contrôler son système nerveux autonome. C’est ainsi que l’on obtient les meilleurs résultats. La relation établie entre le thérapeute et le patient est une relation d’égal à égal ou le patient est parfaitement conscient de tout ce qui se dit et participe activement à la désensibilisation de son traumatisme. Le patient est invité à expérimenter les propositions que lui fait le thérapeute et à mettre en œuvre ses propres idées qui lui permettront de désensibiliser ses traumatismes. La volonté est de travailler les traumatismes avec le minimum de douleurs.'
+      content:'C’est une hypnose conversationnelle où le patient n’est pas endormi mais bien dans un état modifié de conscience dans lequel il est capable de contrôler son système nerveux autonome. C’est ainsi que l’on obtient les meilleurs résultats. La relation établie entre le thérapeute et le patient est une relation d’égal à égal où le patient est parfaitement conscient de tout ce qui se dit et participe activement à la désensibilisation de son traumatisme. Le patient est invité à expérimenter les propositions que lui fait le thérapeute et à mettre en œuvre ses propres idées qui lui permettront de désensibiliser ses traumatismes. La volonté est de travailler les traumatismes avec le minimum de douleur.'
     }
   ]
 
